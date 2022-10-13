ac2d0f2c13d4136696a593f4193b084ee7b64539
Ismael Galarza <underthehaze13@outlook.com>
2022-10-13 17:15:38 -0400

    completed the play round function and added comments

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 // function that determines that computers choice
 
-getComputerChoice = () => {
-  let wordsArr = ["Rock", "Paper", "Scissors"];
-  const gameWords = wordsArr[Math.floor(Math.random() * wordsArr.length)];
-  return gameWords;
+getComputerChoice = (gameWords) => {
+  return (gameWords = Math.floor(Math.random() * gameWords.length));
 };
 console.log(getComputerChoice);
 
 //Function that play a around of rock paper scissors
 
 playRound = (playerSection, computerSection) => {
-
+  if (playerSection == computerSection) {
+    console.log("tie");
+  } else if (playerSection == "rock" && computerSection == "Scissors") {
+    console.log(`${playerSection} beats ${computerSection}`);
+  } else if (playerSection == "paper" && computerSection == "rock") {
+    console.log(`${playerSection} beats ${computerSection}`);
+  }else if (playerSection == "scissors" && computerSection == "paper"){
+    console.log(`${playerSection} beats ${computerSection}`)
+  } else {
+    console.log(`${computerSection} beats ${playerSection}`);
+  }
 };
 const playerSection = "rock";
-const computerSection = getComputerChoice;
+const computerSection = getComputerChoice(["Rock", "Paper", "Scissors"]);
 console.log(playRound(playerSection, computerSection));
 
 //the game function that calls the playRound function
-
-game = (playRound) => {
-    for( let i = 0; i < 5; i++){
-
-    }
-
-}
