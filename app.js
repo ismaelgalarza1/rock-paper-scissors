be723080ee248786bb10685dfb8219bb7bdbde61
Ismael Galarza <underthehaze13@outlook.com>
2022-10-14 00:08:16 -0400

    fixed typos and completed the for loop.

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
+//Declartation of the player and comupter's score for the game
+let playerScore = 0;
+let computerScore = 0;
 // function that determines that computers choice
 
-getComputerChoice = (gameWords) => {
-  return (gameWords = Math.floor(Math.random() * gameWords.length));
+const computerPlay = () => {
+  const computerArr = ["rock", "paper", "scissors"];
+  const randNum = Math.floor(Math.random() * 3);
+  const computerChoice = computerArr[randNum];
+  return computerChoice;
 };
-console.log(getComputerChoice);
-
 //Function that play a around of rock paper scissors
-
-playRound = (playerSection, computerSection) => {
-  if (playerSection == computerSection) {
-    console.log("tie");
-  } else if (playerSection == "rock" && computerSection == "Scissors") {
-    console.log(`${playerSection} beats ${computerSection}`);
-  } else if (playerSection == "paper" && computerSection == "rock") {
-    console.log(`${playerSection} beats ${computerSection}`);
-  }else if (playerSection == "scissors" && computerSection == "paper"){
-    console.log(`${playerSection} beats ${computerSection}`)
+playRound = (playerSelection, computerSelection) => {
+  if (playerSelection == computerSelection) {
+    return "You tied with the computer!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    playerScore++;
+  } else if (
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "paper" && playerSelection === "rock") ||
+    (computerSelection === "scissors" && playerSelection === "paper")
+  ) {
+    computerScore++;
   } else {
-    console.log(`${computerSection} beats ${playerSection}`);
+    return "Please selection the correct weapon";
   }
 };
-const playerSection = "rock";
-const computerSection = getComputerChoice(["Rock", "Paper", "Scissors"]);
-console.log(playRound(playerSection, computerSection));
 
-//the game function that calls the playRound function
+const gamePlay = () => {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt(
+      "choose your weapon",
+      "Rock, Paper, or Scrissors"
+    );
+    const lowerCapPlayerSelection = playerSelection.toLowerCase();
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
+  }
+  if (playerScore > computerScore) {
+    return "You Beat the computer!";
+  } else if (playerScore < computerScore) {
+    return " You Lost, the computer won!";
+  } else {
+    return " you tied with the computer!";
+  }
+};
+//the Game() function that calls the playRound function and keeps track of scores
+console.log(gamePlay());
