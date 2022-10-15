fef537f4a4a1afd0d32ac72cac0e2b3026352a1e
Ismael Galarza <underthehaze13@outlook.com>
2022-10-14 20:46:02 -0400

    Added three buttons for each event

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 /* 
     The Odin Project 
     Name: Ismael Galarza
-    Date: 10-14-2022
-    Project: Rock, Paper, Scrissors 
+    Project: Rock, Paper, Scrissors part 2
     */
+//three buttons that are going to be used for the game 1. playerSelections, computerSelection, and Start button.
+
+let playerBtn = document.createElement("button", "Player");
+playerBtn.onclick = () => {
+  playRound(playerSelection);
+};
+console.log(playerBtn);
+
+let compBtn = document.createElement("button", "Computer");
+compBtn.onclick = () => {
+  playRound(computerSelection);
+};
+console.log(compBtn);
+
+let startBtn = document.createElement("button", "Start");
+startBtn.onclick = () => {
+  playRound();
+};
+console.log(startBtn);
 
 //Declartation of the player and comupter's score for the game
 let playerScore = 0;
@@ -43,24 +61,24 @@ playRound = (playerSelection, computerSelection) => {
 // function that determines the winner through a for loop
 // and used toLowerCase()
 
-const gamePlay = () => {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt(
-      "choose your weapon",
-      "Rock, Paper, or Scrissors"
-    );
-    const computerSelection = computerPlay().toLowerCase();
-    playRound(playerSelection.toLowerCase(), computerSelection);
-  }
-  if (playerScore > computerScore) {
-    return "You Beat the computer!";
-  } else if (playerScore < computerScore) {
-    return " You Lost, the computer won!";
-  } else {
-    return " you tied with the computer!";
-  }
-};
+// const gamePlay = () => {
+//   for (let i = 0; i < 5; i++) {
+//     const playerSelection = prompt(
+//       "choose your weapon",
+//       "Rock, Paper, or Scrissors"
+//     );
+//     const computerSelection = computerPlay().toLowerCase();
+//     playRound(playerSelection.toLowerCase(), computerSelection);
+//   }
+//   if (playerScore > computerScore) {
+//     return "You Beat the computer!";
+//   } else if (playerScore < computerScore) {
+//     return " You Lost, the computer won!";
+//   } else {
+//     return " you tied with the computer!";
+//   }
+// };
 
 //the Game() function that calls the playRound function and keeps track of scores
 
-console.log(gamePlay());
+// console.log(gamePlay());
